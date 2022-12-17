8ca93fdfff02fb1f22bdbd0aadc26938a27ca57f
Mahmoud H. Alim <mahmoud.abdelalim@4sale.tech>
2022-12-17 12:07:47 +0200

    feature/domain_layer_setup
    add PhotoMapper to map from data module photo model to RoverPhoto domain module

---
+++ app/src/main/java/mahmoud/alim/marsrover/domain/mapper/PhotoMapper.kt
@@ -0,0 +1,23 @@
+package mahmoud.alim.marsrover.domain.mapper
+
+import mahmoud.alim.marsrover.data.remote.dto.Photo
+import mahmoud.alim.marsrover.domain.model.RoverPhoto
+import mahmoud.alim.marsrover.domain.model.RoverStatus
+
+/**
+ * @author Mahmoud Alim on 17/12/2022.
+ */
+
+fun Photo.toRoverPhoto(): RoverPhoto {
+    return RoverPhoto(
+        id = id,
+        imageSrc = imgSrc,
+        earthDate = earthDate,
+        cameraName = camera.name,
+        roverId = rover.id,
+        roverName = rover.name,
+        roverLandingDate = rover.landingDate,
+        roverLaunchDate = rover.launchDate,
+        roverStatus = RoverStatus.fromString(rover.status)
+    )
+}
