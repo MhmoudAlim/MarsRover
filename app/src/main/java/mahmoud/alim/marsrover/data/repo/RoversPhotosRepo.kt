ddd34611742299fbc9b2494cc0fc64037f693192
Mahmoud H. Alim <mahmoud.abdelalim@4sale.tech>
2022-12-17 12:09:25 +0200

    feature/domain_layer_setup
    update RoversPhotosRepo getPhotos to return list of domain layer instead and map the result to it

--- app/src/main/java/mahmoud/alim/marsrover/data/repo/RoversPhotosRepo.kt
+++ app/src/main/java/mahmoud/alim/marsrover/data/repo/RoversPhotosRepo.kt
@@ -1,12 +1,12 @@
 package mahmoud.alim.marsrover.data.repo
 
 
-import mahmoud.alim.marsrover.data.remote.dto.Photo
+import mahmoud.alim.marsrover.domain.model.RoverPhoto
 
 /**
  * @author Mahmoud Alim on 16/12/2022.
  */
 interface RoversPhotosRepo {
 
-    suspend fun getPhotos(): Result<List<Photo>>
+    suspend fun getPhotos(): Result<List<RoverPhoto>>
 }
--- app/src/main/java/mahmoud/alim/marsrover/data/repo/RoversPhotosRepoImpl.kt
+++ app/src/main/java/mahmoud/alim/marsrover/data/repo/RoversPhotosRepoImpl.kt
@@ -1,7 +1,8 @@
 package mahmoud.alim.marsrover.data.repo
 
 import mahmoud.alim.marsrover.data.remote.datasource.PhotosDataSrc
-import mahmoud.alim.marsrover.data.remote.dto.Photo
+import mahmoud.alim.marsrover.domain.mapper.toRoverPhoto
+import mahmoud.alim.marsrover.domain.model.RoverPhoto
 
 /**
  * @author Mahmoud Alim on 16/12/2022.
@@ -10,11 +11,11 @@ class RoversPhotosRepoImpl(
     private val remote: PhotosDataSrc
 ) : RoversPhotosRepo {
 
-    override suspend fun getPhotos(): Result<List<Photo>> {
-        return try{
+    override suspend fun getPhotos(): Result<List<RoverPhoto>> {
+        return try {
             val roversDto = remote.getAllPhotos()
-            Result.success(roversDto.photos)
-        }catch (e:Exception){
+            Result.success(roversDto.photos.map { it.toRoverPhoto() })
+        } catch (e: Exception) {
             e.printStackTrace()
             Result.failure(e)
         }
