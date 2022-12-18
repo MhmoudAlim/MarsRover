983b6a5a2b104e3d5b4c7d5c7261564e0c8046f7
Mahmoud H. Alim <mahmoud.abdelalim@4sale.tech>
2022-12-19 00:16:00 +0200

    feature/presentation_layer_setup
    update Kotlin version to match other dependence compatibility

--- buildSrc/src/main/java/Kotlin.kt
+++ buildSrc/src/main/java/Kotlin.kt
@@ -2,7 +2,7 @@
  * @author Mahmoud Alim on 16/12/2022.
  */
 object Kotlin {
-    const val version = "1.7.20"
+    const val version = "1.7.10"
 
     private const val coroutinesVersion = "1.6.0"
     const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
