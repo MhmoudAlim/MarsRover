d916e950774d23f295fe1d63bf41db733ef36c8b
Mahmoud H. Alim <mahmoud.abdelalim@4sale.tech>
2022-12-19 00:49:40 +0200

    feature/ui_layer_setup
    add installSplashScreen to MainActivity

--- app/src/main/java/mahmoud/alim/marsrover/presentation/ui/MainActivity.kt
+++ app/src/main/java/mahmoud/alim/marsrover/presentation/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package mahmoud.alim.marsrover.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import mahmoud.alim.marsrover.databinding.ActivityMainBinding
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
