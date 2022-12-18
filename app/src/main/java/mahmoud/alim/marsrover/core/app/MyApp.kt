b8074c5ecf58521623e31aaf0f21ac84863cbcfa
Mahmoud H. Alim <mahmoud.abdelalim@4sale.tech>
2022-12-19 00:18:27 +0200

    feature/presentation_layer_setup
    add Application class and annotate with HiltAndroidApp

---
+++ app/src/main/java/mahmoud/alim/marsrover/core/app/MyApp.kt
@@ -0,0 +1,12 @@
+package mahmoud.alim.marsrover.core.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author Mahmoud Alim on 19/12/2022.
+ */
+
+@HiltAndroidApp
+class MyApp  : Application(){
+}
