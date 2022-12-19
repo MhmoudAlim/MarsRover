538fcd495b45bbb6e4e5639ebdffc51b7af68d89
Mahmoud H. Alim <mahmoud.abdelalim@4sale.tech>
2022-12-19 17:43:45 +0200

    feature/ui_layer_setup
    implement RoverPhotoDetails mapper fun to map domain model to UI model

--- app/src/main/java/mahmoud/alim/marsrover/domain/mapper/PhotoMapper.kt
+++ app/src/main/java/mahmoud/alim/marsrover/domain/mapper/PhotoMapper.kt
@@ -3,6 +3,7 @@ package mahmoud.alim.marsrover.domain.mapper
 import mahmoud.alim.marsrover.data.remote.dto.Photo
 import mahmoud.alim.marsrover.domain.model.RoverPhoto
 import mahmoud.alim.marsrover.domain.model.RoverStatus
+import mahmoud.alim.marsrover.presentation.ui.model.RoverPhotoDetails
 
 /**
  * @author Mahmoud Alim on 17/12/2022.
@@ -19,5 +20,18 @@ fun Photo.toRoverPhoto(): RoverPhoto {
         roverLandingDate = rover.landingDate,
         roverLaunchDate = rover.launchDate,
         roverStatus = RoverStatus.fromString(rover.status)
+    )
+}
+
+
+fun RoverPhoto.toRoverPhotoDetails(): RoverPhotoDetails {
+    return RoverPhotoDetails(
+        imageSrc = imageSrc,
+        earthDate = earthDate,
+        cameraName = cameraName,
+        roverName = roverName,
+        landingDate = roverLandingDate,
+        launchDate = roverLaunchDate,
+        status = roverStatus.value
     )
 }
